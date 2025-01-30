b9d2d666fd4098cdc2eb5decce630f19e273addb
rizkibagus <brizki121@gmail.com>
2025-01-30 13:13:40 +0700

    feat: add get user location when open this app at Map.jsx

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 import { GOOGLE_MAPS_API_KEY } from '../constants';
 
@@ -6,22 +7,35 @@ const Map = ({ locations }) => {
     googleMapsApiKey: GOOGLE_MAPS_API_KEY,
   });
 
+  // State for storing map center location
+  const [center, setCenter] = useState({ lat: -7.966620, lng: 112.632632 }); // Default: Malang
+
+  // Get user location
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCenter({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        () => {
+          console.warn("Gagal mendapatkan lokasi. Menggunakan lokasi default.");
+        }
+      );
+    }
+  }, []);
+
   if (!isLoaded) return <p>Loading...</p>;
 
   return (
-    <GoogleMap
-      zoom={12}
-      center={{ lat: -7.966620, lng: 112.632632 }}
-      mapContainerClassName="w-full h-96"
-    >
-      {locations.map((loc) => (
-        <Marker
-          key={loc._id}
-          position={{ lat: loc.latitude, lng: loc.longitude }}
-          title={loc.name}
-        />
-      ))}
-    </GoogleMap>
+    <GoogleMap zoom={12} center={center} mapContainerClassName="w-full h-96">
+    {locations.map((loc) => (
+      <Marker key={loc._id} position={{ lat: loc.latitude, lng: loc.longitude }} title={loc.name} />
+    ))}
+  </GoogleMap>
+
   );
 };
 
